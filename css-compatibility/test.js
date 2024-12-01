ae959169cd54695e33d9c9c4a36c398a62273d95
Gupta, Sunny <sunnyvales789@gmail.com>
2024-12-01 09:12:57 +0530

    update

--- css-compatibility/test.js
+++ css-compatibility/test.js
@@ -4,15 +4,48 @@ if('opacity' in document.body.style)
     }
 
 
-    
-// declare var Modernizr;
-
 if (Modernizr.canvas) {
-// let's draw some shapes!
+
+console.log("let's draw some shapes!");
 } else {
-// no native canvas support available :(
+    console.log("no native canvas support available :(");
 }
 
+if (Modernizr.localstorage) {
+    // Code for localStorage
+    console.log('LocalStorage is supported!');
+  } else {
+    // Fallback code if localStorage isn't supported
+    console.log('LocalStorage is not supported!');
+  }
+  
+  // You can check other features as well
+if (Modernizr.flexbox) {
+    console.log('This browser supports Flexbox');
+  } else {
+    console.log('This browser does not support Flexbox');
+  }
+  
+  // Example with CSS Grid detection
+  if (Modernizr.cssgrid) {
+    console.log('This browser supports CSS Grid');
+  } else {
+    console.log('This browser does not support CSS Grid');
+  }
+
+  if (Modernizr.geolocation) {
+    // Browser supports geolocation API
+  } else {
+    // Browser does not support geolocation
+  }
+
+  if (!Modernizr.queryselector) {
+    var script = document.createElement('script');
+    script.src = 'path/to/querySelectorAll-polyfill.js';
+    document.head.appendChild(script);
+  }
+
+  
 // Another way is to use new API CSS.supports. It is supported in most browsers except IE.
 // CSS.supports(property, value)
 console.log(CSS.supports("text-decoration-style", "blink"),
@@ -21,5 +54,4 @@ CSS.supports('--foo', 'red'),
 CSS.supports('(--foo: red)'),
 // CSS.supports(DOMstring)
 CSS.supports("( transform-origin: 5% 5% )"),
-CSS.supports("( transform-style: preserve ) or ( -moz-transform-style: preserve ) or " + "( -o-transform-style: preserve ) or ( -webkit-transform-style: preserve )")
-)
+CSS.supports("( transform-style: preserve ) or ( -moz-transform-style: preserve ) or " + "( -o-transform-style: preserve ) or ( -webkit-transform-style: preserve )"))
